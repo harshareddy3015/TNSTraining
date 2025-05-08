81d89426d654fc62b071a05cb8c87270e7e8b7db
hresh <hresh@LAPTOP-OAGORDKL>
2025-05-08 11:35:44 +0530

    commited first

---
+++ TNS/src/com/tns/oops/Employee.java
@@ -0,0 +1,22 @@
+package com.tns.oops;
+
+
+	//Encapsulation Example  
+	public class Employee {
+			private int eid;
+			private int salary;
+			public int getEid() {
+				return eid;
+			}
+			public void setEid(int eid) {
+				this.eid = eid;
+			}
+			public int getSalary() {
+				return salary;
+			}
+			public void setSalary(int salary) {
+				this.salary = salary;
+			}
+			
+	}
+
